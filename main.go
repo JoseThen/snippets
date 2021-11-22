7862c35644bc5332b1198936e6311c2860f1305a
JoseThen <thenj@protonmail.com>
2021-11-21 20:53:13 -0500

    Chapter 2.4: Adding Routes

--- main.go
+++ main.go
@@ -7,7 +7,23 @@ import (
 
 // Define a handler function for the homepage that writes "Hello from Snippetbox"
 func home(w http.ResponseWriter, r *http.Request) {
+	// Because the "/" path is a catchall and will route here, we need to check if the request is for the homepage
+	if r.URL.Path != "/" {
+		// If it isn't the homepage, then return a 404 not found error
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// showSnippet handler function
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Show a specific snippet"))
+}
+
+// createSnippet handler function
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
 }
 
 func main() {
@@ -15,6 +31,8 @@ func main() {
 	mux := http.NewServeMux()
 	// Use the home function as the handler for the "/" path
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Listening on :4000")
 	// Use http.ListenAndServe to start the server on the specified port using our created mux.
