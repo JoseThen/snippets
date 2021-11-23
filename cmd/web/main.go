6bde9baddca6ba7aa63ba34845f2932fb8dfbf9b
JoseThen <thenj@protonmail.com>
2021-11-22 21:36:51 -0500

    Chapter 3.2: Structured/Leveled Logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,6 +12,13 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	// Parse the command line flag. Need to call before you use the variable.
 	flag.Parse()
+
+	// Make an custom logger for info prefixed with info, and adding some more data like date and time
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
+
+	// Create an error logger, but write to stderr and use `Lshortfile` to include file name and line number
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
+
 	// Create a new ServeMux
 	mux := http.NewServeMux()
 	// Use the home function as the handler for the "/" path
@@ -27,10 +35,20 @@ func main() {
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// Initialize a new http.Server struct. We set the Addr and Handler fields so
+	// that the server uses the same network address and routes as before, and set
+	// the ErrorLog field so that the server now uses the custom errorLog logger in
+	// the event of any problems.
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	// Value returned by `flag.String()` is a pointer, not the actual value so we devalue that with '*'
-	log.Printf("Listening on %s", *addr)
-	// Use http.ListenAndServe to start the server on the specified port using our created mux.
+	infoLog.Printf("Listening on %s", *addr)
+	// Call the ListenAndServe() method on our new http.Server struct.
 	// If it returns an error then log it and exit the program.
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
